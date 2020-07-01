48e7f875f40c7498f1975eaf106ddeeb3a4d64be
6895mahfuzgit <6895mahfuz@gmail.com>
2020-07-02 01:03:04 +0600

    added posts and comment events route and created query project

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const { randomBytes } = require("crypto");
-const cors=require('cors');
+const cors = require("cors");
+const axios = require("axios");
 
 const app = new express();
 app.use(bodyParse.json());
@@ -13,14 +14,29 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentsId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentsId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentsId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log(" from  comment event ", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
