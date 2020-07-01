51c7bb364c910360d09ac942a89d9a479a9be503
6895mahfuzgit <6895mahfuz@gmail.com>
2020-07-02 01:48:05 +0600

    refactor comment list component to receive comments

--- client/src/CommentsList.js
+++ client/src/CommentsList.js
@@ -1,21 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-export default ({ postId }) => {
-  const [commonts, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
-  const renderedComments = commonts.map((comment) => {
+export default ({ comments}) => {
+  const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
 
