5aebb605d8d640460198867e1bc47466bf43d5ea
6895mahfuzgit <6895mahfuz@gmail.com>
2020-07-04 03:47:23 +0600

    Comment moderation feature added

--- moderation/index.js
+++ moderation/index.js
@@ -1,17 +1,27 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const axios=require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-
-const app=new express();
+const app = new express();
 app.use(bodyParser.json());
 
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") {
+    const status = data.content.includes("bad") ? "rejected" : "approved";
 
-app.post("/events",async (req,res)=>{
-
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
 });
 
-
-app.listen(4003,()=>{
- console.log('Moderation listening on port 4003');
+app.listen(4003, () => {
+  console.log("Moderation listening on port 4003");
 });
