b97449c20e185c720dd1ef8d264076105a72bf12
6895mahfuzgit <6895mahfuz@gmail.com>
2020-07-04 15:43:19 +0600

    Dealing with missing events using event storage

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = new express();
 app.use(bodyParser.json());
@@ -12,9 +13,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -37,11 +36,22 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   console.log(posts);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Query listen in port 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event in res.data) {
+    console.log("Processing event ", event.type);
+    handleEvent(event.type, event.data);
+  }
 });
